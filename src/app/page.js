d62b2fa71ad8f2110840d160c64f4564983bacfc
Elgara5364 <steveloardi5364@gmail.com>
2024-02-13 13:06:19 +0900

    Update App and TodoList components

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
+"use client";
+
+import Navbar from "@/components/navbar";
 import TodoList from "@/components/todolist";
 
 export default function Home() {
-  return <TodoList />;
+  console.log("app");
+  return (
+    <div>
+      <Navbar />
+      <TodoList />
+    </div>
+  );
 }
